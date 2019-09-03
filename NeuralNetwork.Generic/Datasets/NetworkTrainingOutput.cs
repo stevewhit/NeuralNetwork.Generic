95d95539cf9f4939c2fc59b78e8a3ddf38b56e26
Steve Whitmire <swhit114@gmail.com>
2019-09-03 13:49:22 -0400

    Update NetworkTrainingOutput.cs

--- NeuralNetwork.Generic/Datasets/NetworkTrainingOutput.cs
+++ NeuralNetwork.Generic/Datasets/NetworkTrainingOutput.cs
@@ -8,11 +8,17 @@ namespace NeuralNetwork.Generic.Datasets
 {
     public interface INetworkTrainingOutput : INetworkOutput
     {
+        /// <summary>
+        /// The expected activation level of this neuron after the inputs have propogated through the network.
+        /// </summary>
         double ExpectedActivationLevel { get; set; }
     }
 
     public class NetworkTrainingOutput : NetworkOutput, INetworkTrainingOutput
     {
+        /// <summary>
+        /// The expected activation level of this neuron after the inputs have propogated through the network.
+        /// </summary>
         public double ExpectedActivationLevel { get; set; }
     }
 }
