567e6106a19855ec275013bdfb9263d2e1760311
stevewhit <swhit114@gmail.com>
2019-09-03 14:36:39 -0400

    Updated property comments and cleaned up unused references.

--- NeuralNetwork.Generic/Datasets/NetworkTrainingIteration.cs
+++ NeuralNetwork.Generic/Datasets/NetworkTrainingIteration.cs
@@ -1,22 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace NeuralNetwork.Generic.Datasets
 {
     public interface INetworkTrainingIteration
     {
+        /// <summary>
+        /// The training inputs for this iteration.
+        /// </summary>
         IList<INetworkTrainingInput> Inputs { get; set; }
+
+        /// <summary>
+        /// The training outputs for this iteration.
+        /// </summary>
         IList<INetworkTrainingOutput> Outputs { get; set; }
+
+        /// <summary>
+        /// The training cost of this iteration.
+        /// </summary>
         double TrainingCost { get; set; }
     }
 
     public class NetworkTrainingIteration : INetworkTrainingIteration
     {
+        /// <summary>
+        /// The training inputs for this iteration.
+        /// </summary>
         public IList<INetworkTrainingInput> Inputs { get; set; }
+
+        /// <summary>
+        /// The training outputs for this iteration.
+        /// </summary>
         public IList<INetworkTrainingOutput> Outputs { get; set; }
+
+        /// <summary>
+        /// The training cost of this iteration.
+        /// </summary>
         public double TrainingCost { get; set; }
 
         public NetworkTrainingIteration()
