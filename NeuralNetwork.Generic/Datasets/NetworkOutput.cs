a813a3fbb62a3f0dd45b8c629cf52587a7381f07
Steve Whitmire <swhit114@gmail.com>
2019-09-03 13:47:44 -0400

    Update NetworkOutput.cs

--- NeuralNetwork.Generic/Datasets/NetworkOutput.cs
+++ NeuralNetwork.Generic/Datasets/NetworkOutput.cs
@@ -8,13 +8,27 @@ namespace NeuralNetwork.Generic.Datasets
 {
     public interface INetworkOutput
     {
+        /// <summary>
+        /// The id of this neuron.
+        /// </summary>
         int NeuronId { get; set; }
+        
+        /// <summary>
+        /// The activation level of this neuron.
+        /// </summary>
         double ActivationLevel { get; set; }
     }
 
     public class NetworkOutput : INetworkOutput
     {
+        /// <summary>
+        /// The id of this neuron.
+        /// </summary>
         public int NeuronId { get; set; }
+        
+        /// <summary>
+        /// The activation level of this neuron.
+        /// </summary>
         public double ActivationLevel { get; set; }
     }
 }
