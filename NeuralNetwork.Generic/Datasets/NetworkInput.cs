e904d9d48f68e51e50875eaf9b7484a791a33559
Steve Whitmire <swhit114@gmail.com>
2019-09-03 13:47:08 -0400

    Update NetworkInput.cs

--- NeuralNetwork.Generic/Datasets/NetworkInput.cs
+++ NeuralNetwork.Generic/Datasets/NetworkInput.cs
@@ -8,13 +8,27 @@ namespace NeuralNetwork.Generic.Datasets
 {
     public interface INetworkInput
     {
+        /// <summary>
+        /// The id of this neuron.
+        /// </summary>
         int NeuronId { get; set; }
+        
+        /// <summary>
+        /// The activation level of this neuron.
+        /// </summary>
         double ActivationLevel { get; set; }
     }
 
     public class NetworkInput : INetworkInput
     {
+        /// <summary>
+        /// The id of this neuron.
+        /// </summary>
         public int NeuronId { get; set; }
+        
+        /// <summary>
+        /// The activation level of this neuron.
+        /// </summary>
         public double ActivationLevel { get; set; }
     }
 }
